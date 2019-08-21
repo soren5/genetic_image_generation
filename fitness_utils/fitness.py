2cfb2e1031728a812bee510764d439b13d1ad330
soren5 <pedrofilipecarvalho@mac.com>
2019-08-21 20:48:43 +0300

    Updated some outdated code

--- fitness_utils/fitness.py
+++ fitness_utils/fitness.py
@@ -15,21 +15,37 @@ def get_fitness(** kwargs):
     y_size = kwargs.get('y_size')
     current_individual = kwargs.get('current_individual')
     current_generation = kwargs.get('current_generation')
-    tf.reset_default_graph()
-    config = tf.ConfigProto(allow_soft_placement=True)
+    tf.compat.v1.reset_default_graph()
+    config = tf.compat.v1.ConfigProto(allow_soft_placement=True)
     config.gpu_options.allow_growth = True
-    with tf.Session(config=config) as sess:
+    with tf.compat.v1.Session(config=config) as sess:
         with sess.graph.device("/gpu:1"):
+            #print(red_tree.get_string())
             red_tensor = tf.cast(red_tree.get_tensor(x_size, y_size), tf.uint8)
+            #print(green_tree.get_string())
             green_tensor = tf.cast(green_tree.get_tensor(x_size, y_size), tf.uint8)
+            #print(blue_tree.get_string())
             blue_tensor = tf.cast(blue_tree.get_tensor(x_size, y_size), tf.uint8)
+            #print(alpha_tree.get_string())
             alpha_tensor = tf.cast(alpha_tree.get_tensor(x_size, y_size), tf.uint8)
             red_result = sess.run(red_tensor)
             green_result = sess.run(green_tensor)
             blue_result = sess.run(blue_tensor)
             alpha_result = sess.run(alpha_tensor)
-            auxiliary_array = [red_result, green_result, blue_result, alpha_result]
+            auxiliary_array = [red_result, green_result, blue_result]
             result = np.stack(auxiliary_array, axis=2)
             matplotlib.pyplot.imsave('generated_images/' + str(current_generation) + '_' + str(current_individual) + '_generated_image.png', result)
     image_size = os.path.getsize('generated_images/' + str(current_generation) + '_' + str(current_individual) + '_generated_image.png')
     return -image_size
+
+if __name__ == "__main__":
+    tf.reset_default_graph()
+    config = tf.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
+        with sess.graph.device("/gpu:1"):
+                a = tf.constant(1)
+                b = tf.constant(2)
+                c = tf.math.maximum(a, b)
+                alpha_result = sess.run(c)
+                print(alpha_result)
